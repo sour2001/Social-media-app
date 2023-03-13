4af8f5a5b426717a790c5092736b0950d047b9fa
sour <dahiya.sourabh444@hmail.com>
2023-03-13 23:41:04 +0530

    displayed comments with post by populating home controller and modifying home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -16,6 +16,13 @@ module.exports.home = function(req, res) {
 
   Post.find({})
     .populate('user')
+    .populate({
+      path: 'comments',
+      populate: {
+        path: 'user'
+      }
+    })
+    .exec()
     .then(posts => {
       return res.render('home', {
         title: 'Codeial || Home',
