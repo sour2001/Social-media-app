b1ebb761f02a656ff005166d716bdb63bcc1783f
sour <dahiya.sourabh444@hmail.com>
2023-03-13 22:58:54 +0530

    Making the post visible to only signined user and using passsport js at posts controller to ensure no one can post without sign-in by changing html and creating their own form

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,17 +1,22 @@
 const Post=require('../models/post');
 
 module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    }).then(function(post){
-        return res.redirect('back');
+    if(req.isAuthenticated()){
+        Post.create({
+            content:req.body.content,
+            user:req.user._id
+        }).then(function(post){
+            return res.redirect('back');
+        }
+        ).catch(function(err){
+            console.log(err);
+            return;
+        }
+        );
     }
-    ).catch(function(err){
-        console.log('Error',err);
-        return;
+    else{
+        return res.redirect('/users/sign-in');
     }
-    );
 }
     
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router=express.Router();
+//we use passport beacuse if a person know the field of our form then he can create a post without signing in by creating a form on home page without sign-in 
+const passport=require('passport');
 const postController=require('../controllers/post_controller');
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 module.exports=router;
 
 
